5b7ebf063e913aab9a65754123b30742e21e4a8c
hossainabrar69 <hossainabrar69@gmail.com>
2022-10-19 14:18:10 +0530

    node version added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component"
+import { AboutComponent } from "./about/about.component"
+import { ProductsComponent } from "./products/products.component"
+import { SingleProductComponent } from "./single-product/single-product.component"
+import { ContactUsComponent } from "./contact-us/contact-us.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"about", component:AboutComponent },
+  { path:"products", component:ProductsComponent },
+  { path:"single-product", component:SingleProductComponent },
+  { path:"contact-us", component:ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
